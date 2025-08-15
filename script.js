2a77d8240be8b1a3592545a10308e5c4df323720
smileywrld <ismaileyyunusa@gmail.com>
2025-08-15 09:03:52 +0100

    fix: correct event listener syntax and update advice title variable

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const adviceId = document.getElementById("advice-id");
+const adviceTitle = document.getElementById("advice-id");
 
 const advice = document.getElementById("advice");
 
 const diceBtn = document.querySelector(".dice-btn");
 
-window.addEventListener("DOMContentLoaded, fetchData");
+window.addEventListener("DOMContentLoaded", fetchData);
 
-function fetchData() {
-    const response = await fetch('')
+async function fetchData() {
+	const response = await fetch("https://api.adviceslip.com/advice");
+	const json = await response.json();
+	console.log(json);
+
+	adviceTitle.innerText = `Advice # ${json.slip.id}`;
+
+	advice.innerText = json.slip.advice;
 }
